3af0c209fbec8e2a47a46a6072ff5ba2d9b544e3
Joey <djrjoey@gmail.com>
2017-01-11 16:00:30 +0100

    Placer un point sur la carte (pour plus tard placer les jeux)

--- PixStreet/app/src/main/java/hpy/pixstreet/MainActivity.java
+++ PixStreet/app/src/main/java/hpy/pixstreet/MainActivity.java
@@ -1,7 +1,6 @@
 package hpy.pixstreet;
 
 import android.content.Intent;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
