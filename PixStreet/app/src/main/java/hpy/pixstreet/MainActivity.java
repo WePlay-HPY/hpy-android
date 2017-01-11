c8372f02c0212fde0fddfb5d1c2dd61b50771500
Joey <djrjoey@gmail.com>
2017-01-11 12:49:49 +0100

    Intégration d’OpenStreetMap dans une activité

--- PixStreet/app/src/main/java/hpy/pixstreet/MainActivity.java
+++ PixStreet/app/src/main/java/hpy/pixstreet/MainActivity.java
@@ -1,7 +1,11 @@
 package hpy.pixstreet;
 
+import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button osm = (Button) findViewById(R.id.osm);
+        osm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OpenStreetMap.class);
+                startActivity(intent);
+            }
+        });
     }
 }
