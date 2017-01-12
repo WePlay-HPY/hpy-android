81a372a9121d033fb930f79f86b6b2ecd3638b5f
Joey <djrjoey@gmail.com>
2017-01-12 11:09:45 +0100

    Ajout des classes MapDatas et MapItem

--- PixStreet/app/src/main/java/hpy/pixstreet/OpenStreetMap.java
+++ PixStreet/app/src/main/java/hpy/pixstreet/OpenStreetMap.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
-import org.osmdroid.google.wrapper.MyLocationOverlay;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -15,14 +14,15 @@ import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.ItemizedOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
-import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class OpenStreetMap extends AppCompatActivity {
 
+    private MapDatas mapDatas = MapDatas.getInstance();
     private MapView map;
     private MyLocationNewOverlay mLocationOverlay;
     private CompassOverlay mCompassOverlay;
@@ -46,7 +46,7 @@ public class OpenStreetMap extends AppCompatActivity {
         mLocationOverlay.enableMyLocation();
         map.getOverlays().add(mLocationOverlay);
 
-        putPoints(new GeoPoint(49.1833, -0.35));
+        putPoints();
     }
 
     public void onResume(){
@@ -59,10 +59,15 @@ public class OpenStreetMap extends AppCompatActivity {
     }
 
 
-    public void putPoints(GeoPoint geo){
-        geo = new GeoPoint(49.1833, -0.35);
+    public void putPoints(){
         ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
-        items.add(new OverlayItem("Here", "SampleDescription", geo));
+        for (Iterator<MapItem> i = mapDatas.getDatasMap().iterator(); i.hasNext(); ) {
+            double longitude = i.next().getLongitude();
+            double latitude  = i.next().getLatitude();
+            GeoPoint geo = new GeoPoint(latitude, longitude);
+            items.add(new OverlayItem("Here", "SampleDescription", geo));
+        }
+
 
         mMyLocationOverlay = new ItemizedIconOverlay<OverlayItem>(items,
                 new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
